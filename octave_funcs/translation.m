1a933c15c9a81a292b166f49ce760647d2a47286
Evan Widloski <evan@Volt.(none)>
2014-08-02 00:21:42 -0400

    added translation.m

---
+++ octave_funcs/translation.m
@@ -0,0 +1,14 @@
+%Evan Widloski 2014-08-01
+%Convert an augmented matrix into the form X = [translation] + [spanning1] + [spanning2] ... + [spanningN]
+
+function translate=translation(v)
+	
+	%
+	[x,y] = find(v');
+	pivot_cols = [1; x(find(diff(x)<0) + 1)];
+	translation = v;
+	%delete pivot cols from matrix
+	translation(:,pivot_cols')=[];
+
+
+endfunction
