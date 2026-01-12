640afe129100e64135426ef696577c715e218886
Evidlo <evan+gh@evanw.org>
2026-01-12 15:18:06 -0600

    add claude usage

---
+++ claude_usage.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+response=$(./secret_claude_fetch_usage.sh)
+
+percent=$(jq --join-output '.["five_hour"]["utilization"]' <<< "${response}")
+
+reset=$(jq --join-output '.["five_hour"]["resets_at"]' <<< "${response}")
+reset_unix=$(date -d "$reset" +%s)
+now_unix=$(date +%s)
+diff_unix=$((reset_unix - now_unix))
+diff_hours=$((diff_unix / 3600))
+diff_minutes=$(((diff_unix % 3600) / 60))
+
+echo "${percent}% (${diff_hours}:${diff_minutes})"
